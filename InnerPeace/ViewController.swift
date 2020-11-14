16e75af73365442a836df6cdc1f7a477b50af2af
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-11-14 22:33:18 +0530

    first commit

---
+++ InnerPeace/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  InnerPeace
+//
+//  Created by PRABALJIT WALIA     on 14/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var background:UIImageView!
+    @IBOutlet weak var quote: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
